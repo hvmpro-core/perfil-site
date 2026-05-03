9b4c373a1aab69c879466b46c024557b6bdfd91e
hvmpro <hewerton_vm@outlook.com>
2026-05-03 14:29:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("https://raw.githubusercontent.com/SEU_USUARIO/perfil-analise/main/resumo.json")
+fetch("https://raw.githubusercontent.com/hvmpro/perfil-analise/main/resumo.json")
   .then(res => res.json())
   .then(data => {
     document.getElementById("repos").innerText = data.total_repos;
